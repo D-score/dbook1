feb4f7c6fd79379dff02b81e2cf31504cf839bb7
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-01-09 22:35:19 +0100

    Set global fig.align = 'center'

--- R/before_book_script.R
+++ R/before_book_script.R
@@ -2,7 +2,6 @@ library(knitr)
 
 # knitr options
 knit_theme$set(knit_theme$get("earendel"))
-opts_chunk$set(comment = NA, background = "#FBFBFB")
 knit_hooks$set(document = function(x) {
   sub('\\usepackage[]{color}', '\\usepackage{xcolor}', x, fixed = TRUE)})
 
