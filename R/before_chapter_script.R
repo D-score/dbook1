263e3b02493b2cc146206908326346ad07ec33f9
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-01-09 17:25:42 +0100

    Remove superflous theme_light()

--- R/before_chapter_script.R
+++ R/before_chapter_script.R
@@ -9,3 +9,4 @@ source("R/hooks.R")
 pkg <- c("knitr", "kableExtra", "ggplot2", "tidyr", "dplyr")
 loaded <- sapply(pkg, require, character.only = TRUE,
                  warn.conflicts = FALSE, quietly = TRUE)
+ggplot2::theme_set(theme_light())
