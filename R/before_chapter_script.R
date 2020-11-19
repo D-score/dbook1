8ed8a9329f33021370c2de8ca43f585594ec81b8
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-11-19 23:49:39 +0100

    Prepare for pdf & html

--- R/before_chapter_script.R
+++ R/before_chapter_script.R
@@ -1,7 +1,7 @@
 # automatically run before each chapter
 
 # load standard packages
-pkg <- c("knitr", "kableExtra", "ggplot2", "tidyr", "dplyr")
+pkg <- c("knitr", "kableExtra", "ggplot2", "tidyr", "dplyr", "RColorBrewer")
 loaded <- sapply(pkg, require, character.only = TRUE,
                  warn.conflicts = FALSE, quietly = TRUE)
 
