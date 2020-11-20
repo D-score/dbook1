443f224481c55ded20caa82315d02a55d7a4c9c7
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-11-20 14:58:33 +0100

    Customise plots in ch4 to latex

--- R/before_book_script.R
+++ R/before_book_script.R
@@ -2,9 +2,9 @@ library(knitr)
 library(kableExtra)
 
 # knitr options
-knit_theme$set(knit_theme$get("earendel"))
 knit_hooks$set(document = function(x) {
-  sub('\\usepackage[]{color}', '\\usepackage{xcolor}', x, fixed = TRUE)})
+  sub('\\usepackage[]{color}', '\\usepackage{xcolor}', x, fixed = TRUE)}
+)
 
 # R options
 options(na.action = na.fail, width = 64, digits = 3, scipen = 6,
