704509a6a790f47e3c21c5d47818ad7e12e592ec
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-11-23 15:00:21 +0100

    Try dynamic.png

--- R/make_transparent_png.R
+++ R/make_transparent_png.R
@@ -3,3 +3,8 @@ library(magick)
 fig <- image_read("fig/Afbeelding1.png")
 f2 <- image_background(fig, "transparent")
 image_write(f2, "fig/lineplot.png")
+
+fig <- image_read("fig/dynamic.png")
+f2 <- image_background(fig, "pink")
+f3 <- image_fill(fig, "pink", point = "+10+10")
+# image_write(f2, "fig/dynamic2.png")
